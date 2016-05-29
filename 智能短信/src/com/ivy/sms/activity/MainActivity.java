1db727a9acbe85c91a9bfc761764c85dcc3fbcb2
IvyZh <app_do@163.com>
2016-05-30 06:19:16 +0800

    x

--- 智能短信/src/com/ivy/sms/activity/MainActivity.java
+++ 智能短信/src/com/ivy/sms/activity/MainActivity.java
@@ -2,14 +2,14 @@ package com.ivy.sms.activity;
 
 import android.view.View;
 
+import com.ivy.sms.R;
 import com.ivy.sms.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
 	@Override
 	public void initView() {
-		// TODO Auto-generated method stub
-		
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
